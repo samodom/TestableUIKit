39edc8fe177eddbd6e137e7c537b32f42d62d7e8
Sam Odom <samodom@me.com>
2014-12-19 21:08:56 -0600

    Initial Commit

---
+++ TestableUIKit/TestableUIKit.h
@@ -0,0 +1,19 @@
+//
+//  TestableUIKit.h
+//  TestableUIKit
+//
+//  Created by Sam Odom on 12/19/14.
+//  Copyright (c) 2014 Swagger Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestableUIKit.
+FOUNDATION_EXPORT double TestableUIKitVersionNumber;
+
+//! Project version string for TestableUIKit.
+FOUNDATION_EXPORT const unsigned char TestableUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestableUIKit/PublicHeader.h>
+
+

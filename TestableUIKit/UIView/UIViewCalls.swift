4adfa84ee452fcf4b47c76e9eebfeebdfbfeea98
Sam Odom <samodom@me.com>
2014-12-21 01:27:43 -0600

    Documented UIView spy method forwarding

--- TestableUIKit/UIView/UIViewCalls.swift
+++ TestableUIKit/UIView/UIViewCalls.swift
@@ -55,10 +55,20 @@ public extension UIView {
 
 extension UIView: ShimMethodForwarding {
 
+    /**
+      This method indicates whether or not the spy for the provided selector forwards the method call to the superclass implementation.
+      @param        selector Selector of spy method to check for forwarding status.
+      @return       Boolean value indicating whether or not the spy currently forwards calls to the specified method.
+    */
     public func shouldForwardMethodCallWithSelector(selector: Selector) -> Bool {
         return blockerList.shouldForwardMethodCallWithSelector(selector)
     }
 
+    /**
+      Calls to this method control whether or not the spy for the provided selector forwards the method call to the superclass implementation.
+      @param        selector Selector of spy method of which to change the forwarding status.
+      @param        Boolean value indicating whether or not the method calls should be forwarded.
+    */
     public func setShouldForwardMethodCallWithSelector(selector: Selector, _ shouldForward: Bool) {
         blockerList.setShouldForwardMethodCallWithSelector(selector, shouldForward)
     }

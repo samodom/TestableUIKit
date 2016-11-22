a2d508bbc2de826cf5b44b6026ad08db5d2869a1
Sam Odom <samodom@icloud.com>
2016-11-22 15:46:16 -0700

    Split responder spy implementation into two files

--- TestableUIKit/UIResponder/UIResponderEvidence.swift
+++ TestableUIKit/UIResponder/UIResponderEvidence.swift
@@ -6,4 +6,83 @@
 //  Copyright © 2016 Swagger Soft. All rights reserved.
 //
 
-import Foundation
+import FoundationSwagger
+
+
+extension UIResponder {
+
+    enum SpyCaptureKeys {
+        private static let calledSuffix = "Called"
+
+        static let becomeFirstResponder = [becomeFirstResponderCalled]
+        static let becomeFirstResponderCalled = ObjectAssociationKey("becomeFirstResponder" + calledSuffix)
+
+        static let resignFirstResponder = [resignFirstResponderCalled]
+        static let resignFirstResponderCalled = ObjectAssociationKey("resignFirstResponder" + calledSuffix)
+    }
+
+}
+
+
+public extension UIResponder {
+
+    enum OriginalSelectors {
+        static let becomeFirstResponder = #selector(UIResponder.becomeFirstResponder)
+        static let resignFirstResponder = #selector(UIResponder.resignFirstResponder)
+    }
+
+    enum SpySelectors {
+        static let becomeFirstResponder = #selector(UIResponder.spy_becomeFirstResponder)
+        static let resignFirstResponder = #selector(UIResponder.spy_resignFirstResponder)
+    }
+
+    public enum SpyAssociations {
+        public static let becomeFirstResponder = MethodAssociation(
+            forClass: UIResponder.self,
+            ofType: .instance,
+            originalSelector: OriginalSelectors.becomeFirstResponder,
+            alternateSelector: SpySelectors.becomeFirstResponder
+        )
+
+        public static let resignFirstResponder = MethodAssociation(
+            forClass: UIResponder.self,
+            ofType: .instance,
+            originalSelector: OriginalSelectors.resignFirstResponder,
+            alternateSelector: SpySelectors.resignFirstResponder
+        )
+        
+    }
+
+}
+
+
+public extension UIResponder {
+
+    /// Indicates whether the `becomeFirstResponder` method has been called on this object.
+    public final var becomeFirstResponderCalled: Bool {
+        get {
+            return booleanAssociation(
+                for: SpyCaptureKeys.becomeFirstResponderCalled,
+                defaultValue: false
+            )
+        }
+        set {
+            associate(newValue, with: SpyCaptureKeys.becomeFirstResponderCalled)
+        }
+    }
+
+
+    /// Indicates whether the `resignFirstResponder` method has been called on this object.
+    public final var resignFirstResponderCalled: Bool {
+        get {
+            return booleanAssociation(
+                for: SpyCaptureKeys.resignFirstResponderCalled,
+                defaultValue: false
+            )
+        }
+        set {
+            associate(newValue, with: SpyCaptureKeys.resignFirstResponderCalled)
+        }
+    }
+
+}
